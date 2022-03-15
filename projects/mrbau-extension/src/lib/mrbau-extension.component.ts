42095a3eeadf7db0270c144529f40002e8761808
Wolfgang Moser <wolfgang.moser@mrbau.at>
2022-03-15 08:34:15 +0100

    Add Fördermanager Link to mrbau-extension.

--- projects/mrbau-extension/src/lib/mrbau-extension.component.ts
+++ projects/mrbau-extension/src/lib/mrbau-extension.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-mrbau-extension',
+  templateUrl: './mrbau-extension.component.html',
+  styleUrls: ['./mrbau-extension.component.scss'],
+
   template: `
+  <div id="main">
+  <h2>Fördermanager</h2>
+
     <p>
       mrbau-extension works!
     </p>
+
+    <span><img class="textIcon" src="./assets/images/ft_ic_folder_shortcut_link.svg" alt="Eigenschaften"> <a href="/#/personal-files/details/{{row.id}}">Link zu den Ordner-Eigenschaften</a></span>
   `,
   styles: [
   ]
