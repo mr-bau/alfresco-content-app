924c9a706369f9ec853bcd27e9d68fb84f001793
Wolfgang Moser <wolfgang.moser@mrbau.at>
2025-04-07 08:45:34 +0200

    Allow delete task for admins

--- app/src/app/custom/tasks-menu-delete/tasks-menu-delete.component.ts
+++ app/src/app/custom/tasks-menu-delete/tasks-menu-delete.component.ts
@@ -7,7 +7,7 @@ import { ITaskChangedData } from '../tasks/tasks.component';
 @Component({
   selector: 'aca-tasks-menu-delete',
   template: `
-    <button *ngIf="!isDisabled()" mat-menu-item (click)="onDeleteTaskClicked()" disabled> <!-- [disabled]="isDisabled()">-->
+    <button *ngIf="!isDisabled()" mat-menu-item (click)="onDeleteTaskClicked()" [disabled]="isDisabled()">
       <mat-icon>delete</mat-icon>
         <span>Aufgabe Löschen</span>
     </button>
